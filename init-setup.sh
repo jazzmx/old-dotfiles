5ba1532a6c458ae886f83691d187330f3e6ebf96
Maxime Langlois <maxime.langlois@grassvalley.com>
2019-01-02 14:42:59 -0500

    Updated init-setup script

--- init-setup.sh
+++ init-setup.sh
@@ -1,30 +1,54 @@
 #!/usr/bin/env bash
-sudo apt install build-essential git cvs g++ clang autoconf automake cmake pkg-config gettext libxml2-utils libevent-dev libncurses5-dev exuberant-ctags xsel colordiff libtool libtool-bin unzip schroot debootstrap
+PKGLIST="
+build-essential
+git
+cvs
+g++
+clang
+autoconf
+automake
+cmake
+pkg-config
+gettext
+libxml2-utils
+libevent-dev
+libncurses5-dev
+exuberant-ctags
+xsel
+colordiff
+libtool
+libtool-bin
+unzip
+schroot
+debootstrap
+samba
+libgtk-3-dev
+python-dev
+python-pip
+python3-dev
+python3-pip
+sublime-text
+meld
+gtkterm
+minicom
+spotify-client
+"
+sudo apt install $PKGLIST
 
+# Add multiarch support
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install libc6:i386 libncurses5:i386 libstdc++6:i386
-
-sudo apt install samba
-sudo apt install libgtk-3-dev
 
 # Neovim
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim
 
-# Python support
-sudo apt-get install python-dev python-pip python3-dev python3-pip
-
 # Clang packages (for Ubuntu 16.04 - https://apt.llvm.org/)
 # Fingerprint: 6084 F3CF 814B 57C1 CF12 EFD5 15CF 4D18 AF4F 7421
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt install clang-4.0 clang-4.0-doc libclang-common-4.0-dev libclang-4.0-dev libclang1-4.0 libclang1-4.0-dbg libllvm-4.0-ocaml-dev libllvm4.0 libllvm4.0-dbg lldb-4.0 llvm-4.0 llvm-4.0-dev llvm-4.0-doc llvm-4.0-examples llvm-4.0-runtime clang-format-4.0 python-clang-4.0 libfuzzer-4.0-dev
-
-# Additional apps
-sudo apt install sublime-text meld gtkterm minicom
-sudo apt install spotify-client
-
 
 #
 # Build packages from sources
@@ -33,6 +57,12 @@ BIN_DIR="$HOME/bin"
 BUILD_DIR="$HOME/build"
 mkdir -p $BIN_DIR $BUILD_DIR
 cd $BUILD_DIR
+
+# ranger
+git clone https://github.com/ranger/ranger.git
+pushd ranger
+sudo make prefix=/usr/local install
+popd
 
 # tmux
 git clone https://github.com/tmux/tmux.git
